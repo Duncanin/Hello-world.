f154792e822dd28b11c3d257f8f21d6393f012dc
Duncan <ken211812@yahoo.com.tw>
2024-12-25 21:14:18 +0800

    Feature-D commit

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, Git!");
         System.out.println("the main line.");
+        System.out.println("new line.");
     }
 }
