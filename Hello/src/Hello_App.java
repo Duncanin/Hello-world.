c09f5e788aea153880e09ead2b6444e3b90a881f
Duncan <ken211812@yahoo.com.tw>
2024-12-25 22:15:40 +0800

    fix conflix

--- Hello/src/Hello_App.java
+++ Hello/src/Hello_App.java
@@ -1,4 +1,4 @@
-public class App {
+public class Hello_App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("that's anothor project for java.");
