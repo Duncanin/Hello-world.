6f62c6dc64b229fe8e818bddb993ea0873763497
Duncan <ken211812@yahoo.com.tw>
2024-12-25 22:25:19 +0800

    add Third line.

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("the main line.");
-        System.out.println("new line.");
+        System.out.println("Hello, Git!");
+        System.out.println("Third line.");
     }
 }
+
