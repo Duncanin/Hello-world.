c7fef2847833d5fdb5705c8220ac086927b5fc7c
Duncan <ken211812@yahoo.com.tw>
2024-12-25 21:15:08 +0800

    main commit

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
-        System.out.println("the main line.");
+        System.out.println("main conflict.");
     }
 }
