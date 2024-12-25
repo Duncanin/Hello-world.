adab4d2532899d296dbc1cda42e3b5a271910b19
Duncan <ken211812@yahoo.com.tw>
2024-12-25 20:58:53 +0800

    b1 third line

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
-        system.out.println("The third line.");
+        System.out.println("The third line.");
     }
 }
