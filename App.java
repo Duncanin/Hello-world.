ff1e0818cb83b697ddd1474d66f6bfe33bdf6b7e
Duncan <ken211812@yahoo.com.tw>
2024-12-25 21:19:08 +0800

    fix conflict

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
-        System.out.println("main conflict.");
+        System.out.println("fix the conflict.");
     }
-}
+
