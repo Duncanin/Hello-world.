84d288fe7a2ec3a81115082eb6e1d23b5d8495da
Duncan <ken211812@yahoo.com.tw>
2024-12-25 21:10:51 +0800

    remove a line

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
-        system.out.println("the main line.");
+        System.out.println("the main line.");
     }
 }
